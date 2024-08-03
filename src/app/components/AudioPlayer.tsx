7770b70dd32c2efea44109bb641653fd01b47362
marianordqvist <marianordqvist@live.se>
2024-08-03 21:26:55 +0200

    style: add icons

--- src/app/components/AudioPlayer.tsx
+++ src/app/components/AudioPlayer.tsx
@@ -1,18 +1,10 @@
 "use client";
-import { CiPlay1, CiPause1 } from "react-icons/ci";
-// import { IoVolumeMediumOutline } from "react-icons/io5";
+import { CiPlay1, CiPause1, CiVolumeHigh, CiVolumeMute } from "react-icons/ci";
 import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../state/store";
 import { play, pause } from "../state/MusicPlayer/MusicPlayerSlice";
 import { useEffect } from "react";
-
-// const sound = {
-//   title: "Card Title",
-//   song: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
-//   imageUrl:
-//     "https://images.unsplash.com/photo-1521417531039-75e91486cc40?q=80&w=2629&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-// };
 
 const AudioPlayer: React.FC = () => {
   const playerRef = useRef<HTMLAudioElement>(null);
@@ -20,9 +12,9 @@ const AudioPlayer: React.FC = () => {
 
   // get state from redux store
   const isPlaying = useSelector((state: RootState) => state.MusicPlayer.isPlaying);
-  // const currentTrack = useSelector(
-  //   (state: RootState) => state.MusicPlayer.currentTrack
-  // );
+  const currentTrack = useSelector(
+    (state: RootState) => state.MusicPlayer.currentTrack
+  );
   // const currentTime = useSelector(
   //   (state: RootState) => state.MusicPlayer.currentTime
   // );
@@ -57,10 +49,13 @@ const AudioPlayer: React.FC = () => {
         <button className="bg-slate-200 p-2" onClick={handlePause}>
           <CiPause1 />
         </button>
-        {/* <button className="bg-slate-200 p-2">
-          <IoVolumeMediumOutline />
+        <button className="bg-slate-200 p-2">
+          <CiVolumeHigh />
         </button>
-        <p>Current track: {currentTrack?.title || "no track selected"}</p> */}
+        <button className="bg-slate-200 p-2">
+          <CiVolumeMute />
+        </button>
+        {/* <p>Current track: {currentTrack?.title || "no track selected"}</p> */}
         <audio ref={playerRef} />
       </div>
     </>
