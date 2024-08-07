a5224cca220229f3a73db501555a8eee13c0a3eb
marianordqvist <marianordqvist@live.se>
2024-08-07 20:11:54 +0200

    style: userAvatar

--- src/app/components/userAvatar.tsx
+++ src/app/components/userAvatar.tsx
@@ -1,18 +1,25 @@
 import { auth } from "../../../authconfig";
 
-export default async function userAvatar() {
+export default async function UserAvatar() {
   const session = await auth();
 
-  if (!session || !session.user) return null;
+  if (!session || !session.user)
+    return (
+      <div className="bg-zinc-200 inline-block w-10 h-10 p-3 rounded-full text-white ">
+        A
+      </div>
+    );
 
   const userImage = session.user.image || "";
-  const userName = session.user.name || "";
 
   return (
     <div>
       <>
-        <img src={userImage} alt="user avatar" />
-        <p>Hello {userName}!</p>
+        <img
+          src={userImage}
+          alt="user avatar"
+          className="inline-block w-10 h-10 rounded-full"
+        />
       </>
     </div>
   );
