cc5d43cad49ed7df0c69b4d826e394080faba973
marianordqvist <marianordqvist@live.se>
2024-08-15 15:38:12 +0200

    fix: auth handle accessToken

--- authconfig.ts
+++ authconfig.ts
@@ -17,5 +17,18 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       // Logged in users are authenticated, otherwise redirect to login page
       return !!auth;
     },
+    async jwt({ token, account, user }) {
+      // Persist the OAuth access token to the token object right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+        // token.refreshToken = account.refresh_token; // Optional, if you need to handle refreshing tokens
+        // token.expiresAt = Date.now() + account.expires_in * 1000; // Expiry time in milliseconds
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
   },
 });
