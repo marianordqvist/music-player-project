aaed734f86e522603ed51f948c39ed0b9c330140
marianordqvist <marianordqvist@live.se>
2024-08-16 15:30:06 +0200

    refactor: remove scopes & clean up unnecessary comments

--- authconfig.ts
+++ authconfig.ts
@@ -4,11 +4,7 @@ import spotify from "next-auth/providers/spotify";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     spotify({
-      authorization: {
-        params: {
-          scope: "user-modify-playback-state playlist-modify-public",
-        },
-      },
+      authorization: {},
     }),
   ],
   pages: { signIn: "/" },
@@ -17,17 +13,16 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       // Logged in users are authenticated, otherwise redirect to login page
       return !!auth;
     },
-    async jwt({ token, account, user }) {
-      // Persist the OAuth access token to the token object right after signin
+    async jwt({ token, account }) {
       if (account) {
         token.accessToken = account.access_token;
-        // token.refreshToken = account.refresh_token; // Optional, if you need to handle refreshing tokens
-        // token.expiresAt = Date.now() + account.expires_in * 1000; // Expiry time in milliseconds
       }
       return token;
     },
     async session({ session, token }) {
-      session.accessToken = token.accessToken;
+      if (typeof token.accessToken === "string") {
+        session.accessToken = token.accessToken;
+      }
       return session;
     },
   },
