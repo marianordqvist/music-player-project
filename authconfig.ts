9f5cabbc4c40b5752db961df958a33016a7f46af
marianordqvist <marianordqvist@live.se>
2024-08-23 09:52:12 +0200

    fix: refresh acessToken added

--- authconfig.ts
+++ authconfig.ts
@@ -1,10 +1,10 @@
-import NextAuth from "next-auth";
+import NextAuth, { type User } from "next-auth";
 import spotify from "next-auth/providers/spotify";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     spotify({
-      authorization: {},
+      authorization: `https://accounts.spotify.com/authorize?scope=user-modify-playback-state+&response_type=code`,
     }),
   ],
   pages: { signIn: "/" },
@@ -15,9 +15,52 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
     async jwt({ token, account }) {
       if (account) {
-        token.accessToken = account.access_token;
+        // First-time login, save the access_token, its expiry and the refresh_token
+        return {
+          ...token,
+          accessToken: account.access_token,
+          expiresAt: account.expires_at,
+          refreshToken: account.refresh_token,
+        };
+      } else if (Date.now() < token.expiresAt * 1000) {
+        // Subsequent logins, but the access_token is still valid
+        return token;
+      } else {
+        // Subsequent logins, but the access_token has expired, try to refresh it
+        if (!token.refresh_token) throw new TypeError("Missing refresh_token");
+
+        try {
+          const response = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            body: new URLSearchParams({
+              grant_type: "refresh_token",
+              refresh_token: token.refresh_token!,
+              client_id: process.env.AUTH_SPOTIFY_ID!,
+            }),
+          });
+
+          const tokensOrError = await response.json();
+
+          if (!response.ok) throw tokensOrError;
+
+          const newTokens = tokensOrError as {
+            access_token: string;
+            expires_in: number;
+            refresh_token?: string;
+          };
+
+          token.accessToken = newTokens.access_token;
+          token.expiresAt = Math.floor(Date.now() / 1000 + newTokens.expires_in);
+          // Some providers only issue refresh tokens once, so preserve if we did not get a new one
+          if (newTokens.refresh_token) token.refresh_token = newTokens.refresh_token;
+          return token;
+        } catch (error) {
+          console.error("Error refreshing access_token", error);
+          // If we fail to refresh the token, return an error so we can handle it on the page
+          token.error = "RefreshTokenError";
+          return token;
+        }
       }
-      return token;
     },
     async session({ session, token }) {
       if (typeof token.accessToken === "string") {
@@ -27,3 +70,18 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   },
 });
+
+declare module "next-auth" {
+  interface Session {
+    error?: "RefreshTokenError";
+  }
+}
+
+declare module "@auth/core/jwt" {
+  interface JWT {
+    access_token: string;
+    expires_at: number;
+    refresh_token?: string;
+    error?: "RefreshTokenError";
+  }
+}
